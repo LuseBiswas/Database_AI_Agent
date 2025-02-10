b7bb87db1b2de1efcb1a172b084de17992d03b93
Ritesh Biswas <lusebiswas@gmail.com>
2025-02-10 14:21:10 +0530

    Frontend URL Updated

--- ai-agent-frontend/ai-agent-frontend/src/api/queryAPI.js
+++ ai-agent-frontend/ai-agent-frontend/src/api/queryAPI.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+// const API_BASE_URL = "http://localhost:3000/api"; This is for local development 
+
+const API_BASE_URL = "https://database-ai-agent-backend.onrender.com/api";
 
 // Function to connect to database
 export const connectToDatabase = async (credentials) => {
